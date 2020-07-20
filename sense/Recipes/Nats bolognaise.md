ab421c4ea7fe6cd24a07e5f3a459daff80ee6192
David Jones <davidthomjones@gmail.com>
2020-07-20 15:17:59 +1000

    Nat's bolognaise added

---
+++ sense/Recipes/Nats bolognaise.md
@@ -0,0 +1,32 @@
+# Nat's What I Reckon - bolagnaise
+
+[Original source](https://www.abc.net.au/news/2020-07-10/nats-what-i-reckon-is-here-to-help-you-make-bolognaise/12402390) and [video](https://youtu.be/Sw_Ze9zIafM)
+
+## Ingredients
+
+- 2 sticks of celery
+- 2 carrots
+- 1 onion
+- 150-200g pancetta (or bacon)
+- Bit over 500g beef mince
+- Bit over 500g pork mince
+- 300g tomato paste
+- 1-2 cups of chicken stock
+- 1 cup of milk
+- 1-2 glasses of wine (red or white)
+- Butter
+- Oil
+- Bay leaves
+- Fresh rosemary, thyme or other savoury herb (optional)
+- Salt and pepper to season
+
+## Method
+
+1. Dice the onion, carrot, celery and pancetta and bung it in a bowl. 
+2. Heat up some butter and a splash of oil in a big pot. When the butter is melted, drop the contents of the bowl in, keep the pan hot, fry until it starts to brown. 
+3. Add beef and pork mince, fry the shit out of it until all the liquid has evaporated. Don't worry if it sticks to the bottom a bit, the wine will sort that out later. 
+4. Add some rosemary if you like. Just chuck in the whole sprig, including the stick. If you prefer to use alternative herbs, use them here instead. 
+5. Keep cooking the liquid out until the meat mix is frying. Once the liquid is fried off, add a glass or two of wine. Let the booze cook off a bit then drop in your tomato paste. 
+6. Add milk. Yeah, it might sound weird but you are on this journey now. Add a cup of milk here and then add your stock. The order is important — stock last!
+7. Season with salt and pepper, add bay leaves, put the lid on then cook it all for as long as you like. The longer the better, but minimum 45 mins. Give it an occasional stir and if it starts to look dry add a bit more stock. 
+8. Remove the rosemary stick and bay leaves when it's done and serve with pasta and fresh grated parmesan cheese. Can be frozen.
--- util/zotero/publicationsPage.py
+++ util/zotero/publicationsPage.py
@@ -1,29 +1,180 @@
 # publicationsPage.py
 # - read content from My Publications in Zotero and write to 
 #   share/My Publications.md
+
+import re
 from pyzotero import zotero
 
 from simple_settings import settings
+
+PAGE_TEMPLATE = """
+# My Publications
+
+A list of PUB_COUNT formal academic publications. My [Google Scholar Profile](http://scholar.google.com/citations?user=k8DkjXIqUl4J) has more detail
+
+| Publication Type | Total |
+| ---------------- | ----- |
+"""
+
+PUBLICATION_TYPE = {
+    'book': 'Authored Book',
+    'bookSection':'Book Chapter',
+    'journalArticle':'Refereed Journal Paper',
+    'conferencePaper':'Refereed Conference Paper',
+    'presentation':'Presentation',
+    'thesis': 'PhD Thesis'
+}
+
+PUBLICATION_ORDER = [ 'book', 'bookSection', 'thesis', 'journalArticle', 
+            'conferencePaper','presentation']
+
+zot = zotero.Zotero(settings.zoteroUserId, 'user', settings.zoteroAPIKey)
 
 #---------------------------------------------------------
 # return an array of full publication details
 def getPublications():
 
     #-- get list of items in publications
-    zot = zotero.Zotero(settings.zoteroUserId, 'user', settings.zoteroAPIKey)
     items = zot.publications()
 #    for item in items: 
 #        print('Item: %s | Key: %s' % (item['data']['itemType'], item['data']['key']))
-
 
     return items
 
     #-- get more detail about each publication
 
+#---------------------------------------------------------
+# counts = calculateCounts( pubs, field)
+# - given dict of publications, return a dict that gives the number of 
+#   items of each field
+
+def calculateCounts( pubs, field='itemType' ):
+    counts = {}
+
+    for item in pubs:
+        if field not in item['data']:
+            continue
+        type = item['data'][field]
+        if type not in counts:
+            counts[type] = 0
+        counts[type]+=1
+
+    return counts
+
+#------------------------------------------------------------
+# list = getPubsField( pubs, field)
+# - return a list containing just the field from the list of pubs
+
+def getPubsField( pubs, field):
+    pubsList = []
+
+    for item in pubs:
+        if field not in item['data']:
+            continue
+        pubsList.append(item['data'][field])
+
+    return pubsList
+
+#---------------------------------------------------------
+# pubs = getYearsPubs( pubs, year)
+# - give list of pubs and a year
+# - return list of pubs from that year
+
+def getYearsPubs( pubs, year ):
+    yearPubs = []
+
+    for item in pubs:
+        if 'date' not in item['data']:
+            continue
+
+        print("-- %s" % item['data']['date'])
+
+        if item['data']['date'] == year:
+            yearPubs.append(item)
+
+    return yearPubs
+
+#--------------------------------------------------------------
+# output = getPubsOutput( pubs )
+# - given a list pubs, return a string show pubs
+
+def getPubsOutput( pubs):
+
+    itemKeys = getPubsField( pubs, "key")
+    print("ITem keys %s" %itemKeys)
+    zot.add_parameters( format="bib", style="apa")
+    output = zot.items( itemKey= itemKeys, format="bib", style="apa")
+
+    print("------------- ouput")
+    print(output)
+    return "Had %s pubs" % len(pubs)
+
+#--------------------------------------------------------------
+# updateFile( types)
+# - write to the file
+
+def updateFile( pubs, types ):
+    pubCount = len(pubs)
+
+    markDown = PAGE_TEMPLATE.replace("PUB_COUNT",str(pubCount))
+
+    #-- generate table of publication type counts
+    for type in PUBLICATION_ORDER:
+        if type not in types or type not in PUBLICATION_TYPE:
+            continue
+        markDown = markDown + "| %s | %s |\n" % (PUBLICATION_TYPE[type],types[type])
+    markDown = markDown + "\n"
+
+    #-- show the by the year complete display
+    yearCounts = calculateCounts( pubs, 'date' )
+    #-- just get the round years
+    years = []
+    for year in yearCounts.keys():
+        print("-- %s" % year)
+        result = re.match( '^[0-9][0-9][0-9][0-9]$', year)
+        if result:
+            years.append(year)
+
+    print( years)
+
+    years.sort( reverse = True)
+    for year in years:
+        markDown = markDown + "## %s\n" %year
+
+        #-- loop through publications for the given year
+        yearPubs = getYearsPubs( pubs, year)
+        print(yearPubs)
+
+        markDown = markDown + getPubsOutput( yearPubs)
+        markDown = markDown + "\n\n"
+
+    print("----------")
+
+    #-- write to the file
+#    with open( settings.myPubsFile, "w") as f:
+#        f.write( markDown)
+#    f.close()
+
+    print( markDown )
+
+
+
 def main(): 
     pubs = getPublications()
 
     print( pubs[0]['data'].keys())
+
+
+    #-- calculate counts of different types - straight python
+    types = calculateCounts( pubs )
+    print( types)
+
+    updateFile( pubs, types)
+
+    #-- display the annual list 
+    #   - including display in format
+    # https://pyzotero.readthedocs.io/en/latest/#read-api-methods
+    # - zot.add_parameters( format="bib") or some such
 
 
 if __name__ == "__main__":
