f68cc6dce85235831d44013555c2d60eb67df93f
David Jones <davidthomjones@gmail.com>
2023-02-26 14:50:10 +1000

    merging rest and xmlrpc

--- util/restBlog.py
+++ util/restBlog.py
@@ -26,6 +26,11 @@ import base64
 import sys
 import io
 
+import markdown
+import toml
+
+from bs4 import BeautifulSoup
+
 import re
 
 from slugify import slugify
@@ -44,6 +49,31 @@ WORDPRESS_HEADER = {'Authorization': 'Basic ' +
                     WORDPRESS_TOKEN.decode('utf-8')}
 
 
+def getFile(markDownFile):
+    with open(markDownFile) as f:
+        markDownData = f.read()
+    f.close()
+
+    print("--------------- original -------------")
+    print(markDownData)
+#    print("--------------- HTML -------------")
+#    print(markdown.markdown(markDownData))
+
+    #x = re.match(".*^toml(.*)```(.*)", markDownData, re.MULTILINE | re.DOTALL  )
+    # x = re.match("#.*^```toml(.*)```", markDownData, re.DOTALL  )
+    x = re.match("```toml([^`]*)```(.*)", markDownData,
+                 re.MULTILINE | re.DOTALL)
+    # x = re.match("#^```(toml)", markDownData, re.MULTILINE  )
+    print(x)
+    print("TOML")
+    print(x[1])
+    print("TOML")
+
+    postConfig = toml.loads(x[1])
+    postContent = x[2]
+
+    return (postConfig, postContent)
+
 def read_wordpress_posts():
     api_url = 'https://djon.es/blog/wp-json/wp/v2/posts?page=1&per_page=100'
     response = requests.get(api_url)
@@ -51,7 +81,7 @@ def read_wordpress_posts():
     print(response_json)
 
 
-def create_wordpress_post( postDetails ):
+def create_wordpress_post(postDetails):
     """
     @function create_wordpress_post
     @param postDetails: dict of post details
@@ -66,9 +96,11 @@ def create_wordpress_post( postDetails ):
 #        'content': 'This is the content of the post'
 #    }
 
-    response = requests.post(api_url, headers=WORDPRESS_HEADER, json=postDetails)
+    response = requests.post(
+        api_url, headers=WORDPRESS_HEADER, json=postDetails)
 
     return response
+
 
 def generateWordPressPost():
     """ 
@@ -84,12 +116,38 @@ def generateWordPressPost():
         'title': title,
         'status': 'draft',     # publish, future, draft, pending, private
         'slug': slugify(title),
-        'categories': [ "bad", "casa" ],
-        #'tags': [],
+        'categories': ["bad", "casa"],
+        # 'tags': [],
         'content': 'This is the content of the post'
     }
 
     return postDetails
+
+
+def transformMarkdownPost():
+    if len(sys.argv) == 2: 
+        markDownFile = sys.argv[1]
+
+    (config, content) = getFile(markDownFile)
+
+    quit()
+"""
+    html = markdown.markdown(content)
+
+    wikiLinks = extractWikiLinks(content)
+    if len(wikiLinks) > 0:
+        html = changeWikiLinks(html, wikiLinks)
+
+        return html
+"""
+#        print(content)
+
+#        print("=================================")
+#        print(html)
+
+#    if "img_base_url" in config:
+        #            print("**** change image base url %s"%config['img_base_url'])
+#        html = changeImgURL(html, config['img_base_url'])
 
 
 def main():
@@ -99,11 +157,14 @@ def main():
     print(f"WORDPRESS_USER={WORDPRESS_USER}")
     print(f"WORDPRESS_PASSWD={WORDPRESS_PASSWD}")
 
-    #read_wordpress_posts()
+    html = transformMarkdownPost()
+    print(html)
+    quit()
+    # read_wordpress_posts()
     postDetails = generateWordPressPost()
     pprint(postDetails)
     #response = create_wordpress_post(postDetails)
-    #print(response)
+    # print(response)
 
 
 if __name__ == "__main__":
